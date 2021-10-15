3ea326b65e2b62be5189e4efd18a62da8a8612ac
Vyrek <horny.martin@email.cz>
2021-10-15 15:28:54 +0200

    Fixed a crashing bug

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Martin/Desktop/OpenGL-master
-# Build directory: C:/Users/Martin/Desktop/OpenGL-master/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
